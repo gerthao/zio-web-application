9f78b78424c43c2ddcd3d9553f6966dedb0e20df
Ger Thao <g.thao19@gmail.com>
2025-08-30 23:21:55 -0500

    add create and getById test cases for ReviewServiceSpec.scala

--- modules/server/src/main/scala/com/rockthejvm/reviewboard/CompanyRepositoryDemo.scala
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/CompanyRepositoryDemo.scala
@@ -7,14 +7,15 @@ import io.getquill.jdbczio.Quill
 import zio.*
 
 object CompanyRepositoryDemo extends ZIOAppDefault:
-    val program: ZIO[CompanyRepository, Throwable, Unit] = for 
-        repo <- ZIO.service[CompanyRepository]
-        _ <- repo.create(Company(-1L, "hello-world", "Hello World", "helloworld.org"))
-    yield ()
+    val program: ZIO[CompanyRepository, Throwable, Unit] =
+        for
+            repo <- ZIO.service[CompanyRepository]
+            _ <- repo.create(Company(-1L, "hello-world", "Hello World", "helloworld.org"))
+        yield ()
 
-    override def run: ZIO[Any & ZIOAppArgs & Scope, Any, Any] = program.provide(
-        CompanyRepositoryLive.layer,
-        Quill.Postgres.fromNamingStrategy(SnakeCase),
-        Quill.DataSource.fromPrefix("application.db")
-    )
-        
+    override def run: ZIO[Any & ZIOAppArgs & Scope, Any, Any] =
+        program.provide(
+            CompanyRepositoryLive.layer,
+            Quill.Postgres.fromNamingStrategy(SnakeCase),
+            Quill.DataSource.fromPrefix("application.db")
+        )
--- modules/server/src/test/scala/com/rockthejvm/reviewboard/services/ReviewServiceSpec.scala
+++ modules/server/src/test/scala/com/rockthejvm/reviewboard/services/ReviewServiceSpec.scala
@@ -1,8 +1,106 @@
 package com.rockthejvm.reviewboard.services
 
-import zio.Scope
-import zio.test.{Spec, TestEnvironment, ZIOSpecDefault}
+import com.rockthejvm.reviewboard.domain.data.Review
+import com.rockthejvm.reviewboard.http.requests.CreateReviewRequest
+import com.rockthejvm.reviewboard.repositories.ReviewRepository
+import zio.{Scope, Task, ULayer, ZIO, ZLayer}
+import zio.test.{Spec, TestEnvironment, ZIOSpecDefault, assertTrue, suite}
 
 object ReviewServiceSpec extends ZIOSpecDefault:
-    override def spec: Spec[TestEnvironment & Scope, Any] = ???
+    val goodReview = Review(
+        id = 1L,
+        companyId = 1L,
+        userId = 1L,
+        management = 5,
+        culture = 5,
+        salary = 5,
+        benefits = 5,
+        wouldRecommend = 10,
+        review = "all good",
+        created = java.time.Instant.now(),
+        updated = java.time.Instant.now(),
+    )
+
+    val badReview = Review(
+        id = 2L,
+        companyId = 1L,
+        userId = 1L,
+        management = 1,
+        culture = 1,
+        salary = 1,
+        benefits = 1,
+        wouldRecommend = 1,
+        review = "not good",
+        created = java.time.Instant.now(),
+        updated = java.time.Instant.now(),
+    )
+
+    val stubRepoLayer: ULayer[ReviewRepository] = ZLayer.succeed:
+        new ReviewRepository:
+            override def create(review: Review): Task[Review] = ZIO.succeed:
+                goodReview
+
+            override def getById(id: Long): Task[Option[Review]] = ZIO.succeed:
+                id match
+                    case 1 => Some(goodReview)
+                    case 2 => Some(badReview)
+                    case _ => None
+
+            override def getByCompanyId(companyId: Long): Task[List[Review]] = ZIO.succeed:
+                if (companyId == 1) then List(goodReview, badReview) else List.empty
+
+            override def getByUserId(userId: Long): Task[List[Review]] = ZIO.succeed:
+                if (userId == 1) then List(goodReview, badReview) else List.empty
+
+            // not used but needed
+            override def update(id: Long, op: Review => Review): Task[Review] =
+                getById(id).someOrFail(new RuntimeException(s"id $id not found")).map(op)
+
+            // not used but needed
+            override def delete(id: Long): Task[Review] =
+                getById(id).someOrFail(new RuntimeException(s"id $id not found"))
+
+    override def spec: Spec[TestEnvironment & Scope, Any] =
+        val testCreate = test("create"):
+            for
+                service <- ZIO.service[ReviewService]
+                review <- service.create(
+                    req = CreateReviewRequest(
+                        companyId = goodReview.companyId,
+                        management = goodReview.management,
+                        culture = goodReview.culture,
+                        salary = goodReview.salary,
+                        benefits = goodReview.benefits,
+                        wouldRecommend = goodReview.wouldRecommend,
+                        review = goodReview.review
+                    ),
+                    userId = 1L
+                )
+            yield assertTrue(
+                review.companyId == goodReview.companyId,
+                review.management == goodReview.culture,
+                review.culture == goodReview.culture,
+                review.salary == goodReview.salary,
+                review.benefits == goodReview.benefits,
+                review.wouldRecommend == goodReview.wouldRecommend,
+                review.review == goodReview.review
+            )
+
+        val testGetById = test("getById"):
+            for
+                service <- ZIO.service[ReviewService]
+                review <- service.getById(1L)
+                reviewNotFound <- service.getById(999L)
+            yield assertTrue(
+                review.contains(goodReview),
+                reviewNotFound.isEmpty
+            )
+
+        suite("ReviewServiceTestSuite")(
+            testCreate,
+            testGetById,
+        ).provide(
+            ReviewServiceLive.layer,
+            stubRepoLayer
+        )
     
