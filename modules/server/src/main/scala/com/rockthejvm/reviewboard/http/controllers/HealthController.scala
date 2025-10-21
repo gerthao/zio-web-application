e80f8f4d4878ec1e602c0048d0dcc5994a64083c
Ger Thao <g.thao19@gmail.com>
2025-10-21 17:22:02 -0500

    reformat code

--- modules/server/src/main/scala/com/rockthejvm/reviewboard/http/controllers/HealthController.scala
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/http/controllers/HealthController.scala
@@ -10,8 +10,8 @@ class HealthController private extends BaseController with HealthEndpoints:
     val health: ServerEndpoint[Any, Task] = healthEndpoint.serverLogicSuccess[Task]: _ =>
         ZIO.succeed("All good!")
 
-    val errorRoute: ServerEndpoint[Any, Task] = errorEndpoint.serverLogic[Task]:
-        _ => ZIO.fail(new RuntimeException("Something went wrong!")).either
+    val errorRoute: ServerEndpoint[Any, Task] = errorEndpoint.serverLogic[Task]: _ =>
+        ZIO.fail(new RuntimeException("Something went wrong!")).either
 
     override val routes: List[ServerEndpoint[Any, Task]] = List(
         health,
@@ -20,4 +20,3 @@ class HealthController private extends BaseController with HealthEndpoints:
 
 object HealthController:
     val makeZIO: ZIO[Any, Nothing, HealthController] = ZIO.succeed(new HealthController)
-    
